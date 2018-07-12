dba913ba530109a57e8881a0294679f436eaf5cb
jnentwig <34762494+jnentwig@users.noreply.github.com>
2018-07-12 17:50:55 -0500

    Create MakeSonicProgram.cpp

---
+++ MakeSonicProgram.cpp
@@ -0,0 +1,13 @@
+//this program takes two text files (the cantus firmus and the species counterpoint)
+//and creates a txt file that can work as a program in sonic pi.
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+int main() {
+	using namespace std;
+
+	ifstream cf_stream;
+	ifstream cp_stream;
+	ofstream pi_stream;
+}
