c604dc3987a85b69bf37fb1a9cfef423eaa824a6
jnentwig <34762494+jnentwig@users.noreply.github.com>
2018-07-16 17:49:16 -0500

    Update MakeSonicProgram.cpp

--- MakeSonicProgram.cpp
+++ MakeSonicProgram.cpp
@@ -3,11 +3,35 @@
 #include <fstream>
 #include <iostream>
 #include <vector>
+using namespace std;
 
+vector<int> Fillvector(ifstream& stream) {
+	vector<int> a;
+	for (int tmp; stream >> tmp; a.push_back(tmp));
+	return a;
+}
+int convertNote(int n) {
+	//middle c is 60 for rasp. pi
+	//21 in my program
+	return n + 39;
+}
 int main() {
-	using namespace std;
-
 	ifstream cf_stream;
 	ifstream cp_stream;
 	ofstream pi_stream;
+
+	cf_stream.open("CFInput.txt");
+	cp_stream.open("CounterpointInput.txt");
+	pi_stream.open("SonicProgram.txt");
+
+	vector<int> CF = Fillvector(cf_stream);
+	vector<int> CP = Fillvector(cp_stream);
+	for (int i = 0; i < CP.size(); i++) {
+		pi_stream << "play " << convertNote(CF[i]) << '\n' << 
+			"play " << convertNote(CP[i]) << '\n' << "sleep 0.5" << endl;//play both notes than sleep
+	}
+
+	cout << "Program is done running, press any key to continue" << endl;
+	char dummy;
+	cin >> dummy;
 }
